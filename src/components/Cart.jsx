2abed6261450a9dd9db0c24aeb0364be970acd96
Ryan Lind <tuk40017@temple.edu>
2024-03-25 19:31:57 -0400

    Branch contains code formatter with script

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -36,7 +36,7 @@ const Cart = ({ modalOpen, setModalOpen, cartItems }) => {
 
     if (!cartItems.every((item) => item.name && item.ingredients)) {
       console.log(
-        "One or more items in cartItems does not have a name or ingredients"
+        "One or more items in cartItems does not have a name or ingredients",
       );
       return;
     }
@@ -70,7 +70,7 @@ const Cart = ({ modalOpen, setModalOpen, cartItems }) => {
         `Users/${user.uid}/Orders`,
         selectedDate,
         { recipes: orderData },
-        "order"
+        "order",
       );
       console.log("Order created successfully");
       //removing all items from cart after an order is placed
