c9466e3160682eebb571e63440216c77f71627ad
sdutill <shawndutill@gmail.com>
2024-03-27 12:19:10 -0400

    Removing useless stuff from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [toggle, setToggle] = useState(false);
-  function buttonPushed() {
-    setToggle((toggle) => !toggle);
-  }
-
   return (
     <div>
       <h1>Welcome to CookBook Pro</h1>
