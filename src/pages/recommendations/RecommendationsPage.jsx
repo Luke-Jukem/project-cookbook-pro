9c07a354568db5edb596659ad5138afa5d3290bb
sdutill <shawndutill@gmail.com>
2024-03-27 12:55:31 -0400

    Refactored Project, created new page structure maintained across all Private pages

---
+++ src/pages/recommendations/RecommendationsPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Recommendations from "./components/Recommendations";
+import UserRecipesViewer from "../../components/UserRecipesViewer";
+
+const RecommendationsPage = () => {
+  return (
+    <div>
+      <div className="main-container">
+        <div className="sidebar-container">
+          <UserRecipesViewer />
+        </div>
+        <div className="content-container">
+          {/* This is where you put the 'Page" file you made previously */}
+          <Recommendations />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecommendationsPage;
