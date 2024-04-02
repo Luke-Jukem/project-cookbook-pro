754cbdb2f082c09d3bb6980e232fe5a0c4cdf479
Evan Dorsey <dorseyevan@gmail.com>
2024-04-02 09:42:22 -0400

    user can save goals to profile, need to fix restrictions with maximum goal limits and more

--- src/pages/health/components/MacroGoalForm.jsx
+++ src/pages/health/components/MacroGoalForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useAuth } from "../../../utils/AuthContext.js";
+import { GoalForm } from "../../../customObjects/GoalForm.js";
 import FirestoreService from "../../../firebase/FirebaseService.js";
 import MappedInputFieldsForm from "../../../pages/create-recipe/components/MappedInputFieldsForm.jsx";
 import "../macroGoals.css";
@@ -9,36 +10,45 @@ const MacroGoalForm = () => {
 
   const [goalFormData, setGoalFormData] = useState({});
   const goalsFields = [
-    { name: "id", label: "ID", type: "text", placeholder: "Enter Form ID" },
     {
       name: "caloriesGoal",
       label: "Calorie Goal (cal)",
       type: "number",
       placeholder: "Enter Calorie Goal",
+      min: 0,
+      max: 12000
     },
     {
       name: "proteinGoal",
       label: "Protein Goal (g)",
       type: "number",
       placeholder: "Enter Protein Goal",
+      min: 0,
+      max: 1000
     },
     {
       name: "carbGoal",
       label: "Carbohydrate Goal (g)",
       type: "number",
       placeholder: "Enter Carbohydrate Goal",
+      min: 0,
+      max: 1000
     },
     {
       name: "fatGoal",
       label: "Fat Goal (g)",
       type: "number",
       placeholder: "Enter Fat Goal",
+      min: 0,
+      max: 1000
     },
     {
       name: "sugarGoal",
       label: "Sugar Goal (g)",
       type: "number",
       placeholder: "Enter Sugar Goal",
+      min: 0,
+      max: 1000
     },
   ];
 
@@ -50,42 +60,35 @@ const MacroGoalForm = () => {
       return;
     }
 
-    const goalsObject = new goalsFields(
+    const goalsObject = new GoalForm(
       goalFormData.caloriesGoal,
-      goalFormData.fat,
-      recipeFormData.id,
-      "", // image is an empty string for now
-      ingredientObjects,
-      [], // instructions is an empty array for now
-      recipeFormData.name,
-      recipeFormData.servings,
-      recipeFormData.summary
+      goalFormData.proteinGoal,
+      goalFormData.carbGoal,
+      goalFormData.fatGoal,
+      goalFormData.sugarGoal,
     );
 
-    // Write the new Recipe to the User's CustomRecipes collection
-    console.log("Recipe Object:", goalsObject);
+    console.log("goals Object:", goalsObject);
 
-    // const collectionPath = `Users/${user.uid}/CustomRecipes`;
-    // const documentId = recipeObject.id;
-    // const dataType = "recipe";
-    // try {
-    //   await FirestoreService.createDocument(
-    //     collectionPath,
-    //     documentId,
-    //     recipeObject,
-    //     dataType,
-    //   );
-    // } catch (error) {
-    //   console.error("Error creating document:", error);
-    // }
+    const collectionPath = `Users/${user.uid}/Health`;
+    const documentId = `${user.uid}.HealthGoals`;
+    const dataType = "goalsResponse";
+    try {
+      await FirestoreService.createDocument(
+        collectionPath,
+        documentId,
+        goalsObject,
+        dataType
+      );
+    } catch (error) {
+      console.error("Error creating document:", error);
+    }
 
     console.log(goalFormData);
     // Object is returned in console
     setIsVisible(false);
   }
 
-
-  
   const handleEdit = () => {
     setIsVisible(true);
   };
@@ -99,14 +102,13 @@ const MacroGoalForm = () => {
     );
   }
 
-
   return (
     <div className="macro-form-container">
       <br />
       <h3>Enter your desired macronutrients below:</h3>
       <p>(You'll be able to go back and edit them later!)</p>
       <br />
-      <form className="input-form-container">
+      <div className="input-form-container">
         <MappedInputFieldsForm
           fields={goalsFields}
           formData={goalFormData}
@@ -124,7 +126,7 @@ const MacroGoalForm = () => {
         >
           Submit
         </button>
-      </form>
+      </div>
     </div>
   );
 };
