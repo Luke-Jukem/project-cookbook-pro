48df915c05f87619c43af6148f19b1fffe2e30b1
Evan Dorsey <dorseyevan@gmail.com>
2024-04-03 12:29:54 -0400

    added DisplayGoals.jsx

---
+++ src/pages/health/components/DisplayGoals.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useAuth } from "../../../utils/AuthContext.js";
+import FirestoreService from "../../../firebase/FirebaseService.js";
+import FirestoreListener from "../../../firebase/FirestoreListener.js";
+
+function DisplayGoals() {
+  const [goals, setGoals] = useState({});
+
+  const { user } = useAuth();
+  const firestoreListener = new FirestoreListener();
+
+  useEffect(() => {
+    if (user) {
+      const userSavedGoalsPath = `Users/${user.uid}/Health/${user.uid}.HealthGoals`;
+      console.log(userSavedGoalsPath);
+
+      const fetchGoals = firestoreListener.subscribeToDocument(
+        userSavedGoalsPath,
+        (doc) => {
+          if (doc.exists) {
+            const userData = doc.data();
+            setGoals(userData.goals);
+          }
+        },
+      );
+
+      return fetchGoals;
+    }
+  }, [user]);
+
+  return (
+    <div>
+      <h2>Goals</h2>
+      <ul>
+        <li>Calories Goal: {goals?.calories || "N/A"}</li>
+        <li>Protein Goal: {goals?.protein || "N/A"}</li>
+        <li>Carbohydrate Goal: {goals?.carbs || "N/A"}</li>
+        <li>Fat Goal: {goals?.fat || "N/A"}</li>
+        <li>Sugar Goal: {goals?.sugar || "N/A"}</li>
+      </ul>
+    </div>
+  );
+}
+
+export default DisplayGoals;
