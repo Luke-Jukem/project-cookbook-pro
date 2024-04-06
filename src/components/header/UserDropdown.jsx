18871a85c0eacb455f5304642bfe6f124b8b2b19
sdutill <shawndutill@gmail.com>
2024-04-06 16:36:11 -0400

    Added OrderHistory to UserDropdown

--- src/components/header/UserDropdown.jsx
+++ src/components/header/UserDropdown.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import { Dropdown } from "react-bootstrap";
 import { useAuth } from "../../utils/AuthContext";
@@ -15,7 +16,9 @@ function UserDropdown() {
       <Dropdown.Menu>
         <Dropdown.Item href="#/action-1">Action 1</Dropdown.Item>
         <Dropdown.Item href="#/action-2">Action 2</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Action 3</Dropdown.Item>
+        <Dropdown.Item as={Link} to="/order-history">
+          Order History
+        </Dropdown.Item>
         <Dropdown.Item onClick={logoutUser}>Logout</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
