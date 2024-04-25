3038069777c36ef416d46429690a9800889de1f9
Evan Dorsey <dorseyevan@gmail.com>
2024-04-25 10:52:00 -0400

    Displays macros, but only by recipe for now

--- src/pages/health/Health.jsx
+++ src/pages/health/Health.jsx
@@ -4,14 +4,12 @@ import MacroGoalForm from "./components/MacroGoalForm.jsx";
 import { useAuth } from "../../utils/AuthContext.js";
 import FirestoreListener from "../../firebase/FirestoreListener.js";
 
-
 const Health = ({ recipes }) => {
-  
   const { user } = useAuth();
   const firestoreListener = new FirestoreListener();
-  
 
   const [showGoals, setShowGoals] = useState(true);
+  const [recipeDetails, setRecipeDetails] = useState({});
 
   console.log(recipes);
 
@@ -34,6 +32,36 @@ const Health = ({ recipes }) => {
     }
   }, []);
 
+  const fetchRecipeDetails = async (recipeId) => {
+    const apiKey = "c70a0f5d5e1f4e18bb55c4bfbc94ab1c";
+    const url = `https://api.spoonacular.com/recipes/${recipeId}/nutritionWidget.json?apiKey=${apiKey}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      let sugar = "N/A";
+      for (const item of data.bad) {
+        if (item.title === "Sugar") {
+          sugar = item.amount;
+          break;
+        }
+      }
+
+      const nutritionInfo = {
+        calories: data.calories,
+        carbohydrates: data.carbs,
+        protein: data.protein,
+        sugar: sugar,
+        fat: data.fat,
+      };
+
+      setRecipeDetails(nutritionInfo);
+    } catch (error) {
+      console.error("Error fetching recipe details:", error);
+    }
+  };
+
   return (
     <div>
       {showGoals ? (
@@ -52,11 +80,16 @@ const Health = ({ recipes }) => {
           <div key={index}>
             <p>Recipe Name: {recipe.name}</p>
             <p>Recipe ID: {recipe.id}</p>
+            <button onClick={() => fetchRecipeDetails(recipe.id)}>
+              Get Recipe Details
+            </button>
             {/* Display other recipe details */}
           </div>
         ))}
       </div>
       <br />
+      {/* Display recipe details */}
+      <pre>{JSON.stringify(recipeDetails, null, 2)}</pre>
     </div>
   );
 };
