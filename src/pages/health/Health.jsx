d15af1616690f949a58962d75826235c59eb7de1
Evan Dorsey <dorseyevan@gmail.com>
2024-04-25 11:01:45 -0400

    Displays total macros

--- src/pages/health/Health.jsx
+++ src/pages/health/Health.jsx
@@ -10,6 +10,14 @@ const Health = ({ recipes }) => {
 
   const [showGoals, setShowGoals] = useState(true);
   const [recipeDetails, setRecipeDetails] = useState({});
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [totalMacros, setTotalMacros] = useState({
+    calories: 0,
+    carbohydrates: 0,
+    protein: 0,
+    sugar: 0,
+    fat: 0,
+  });
 
   console.log(recipes);
 
@@ -47,19 +55,38 @@ const Health = ({ recipes }) => {
           break;
         }
       }
-
       const nutritionInfo = {
-        calories: data.calories,
-        carbohydrates: data.carbs,
-        protein: data.protein,
-        sugar: sugar,
-        fat: data.fat,
+        calories: parseFloat(data.calories),
+        carbohydrates: parseFloat(data.carbs),
+        protein: parseFloat(data.protein),
+        sugar: sugar !== "N/A" ? parseFloat(sugar) : "N/A",
+        fat: parseFloat(data.fat),
       };
 
       setRecipeDetails(nutritionInfo);
+
+      // Update total macros
+      setTotalMacros((prevTotalMacros) => ({
+        calories: prevTotalMacros.calories + nutritionInfo.calories,
+        carbohydrates:
+          prevTotalMacros.carbohydrates + nutritionInfo.carbohydrates,
+        protein: prevTotalMacros.protein + nutritionInfo.protein,
+        sugar:
+          nutritionInfo.sugar !== "N/A"
+            ? prevTotalMacros.sugar + nutritionInfo.sugar
+            : prevTotalMacros.sugar,
+        fat: prevTotalMacros.fat + nutritionInfo.fat,
+      }));
     } catch (error) {
       console.error("Error fetching recipe details:", error);
     }
+  };
+
+  const fetchAllRecipeDetails = () => {
+    recipes.forEach((recipe) => {
+      fetchRecipeDetails(recipe.id);
+    });
+    setButtonClicked(true);
   };
 
   return (
@@ -80,16 +107,23 @@ const Health = ({ recipes }) => {
           <div key={index}>
             <p>Recipe Name: {recipe.name}</p>
             <p>Recipe ID: {recipe.id}</p>
-            <button onClick={() => fetchRecipeDetails(recipe.id)}>
-              Get Recipe Details
-            </button>
             {/* Display other recipe details */}
           </div>
         ))}
+        <button onClick={fetchAllRecipeDetails} disabled={buttonClicked}>
+          Get your Recipe's Macros
+        </button>
       </div>
       <br />
-      {/* Display recipe details */}
-      <pre>{JSON.stringify(recipeDetails, null, 2)}</pre>
+      {/* Display total macros */}
+      <div>
+        <h3>Total Macros:</h3>
+        <p>Calories: {totalMacros.calories}</p>
+        <p>Carbohydrates: {totalMacros.carbohydrates}</p>
+        <p>Protein: {totalMacros.protein}</p>
+        <p>Sugar: {totalMacros.sugar}</p>
+        <p>Fat: {totalMacros.fat}</p>
+      </div>
     </div>
   );
 };
