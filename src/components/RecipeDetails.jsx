4fde0c803f32d0629b9ee9efe44ed324efac4822
sdutill <shawndutill@gmail.com>
2024-05-01 22:15:41 -0400

    Altered this so we don't get the "key" error, made it so that "image" is written to DB

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -11,14 +11,13 @@ import DOMPurify from "dompurify";
 import customRecipeImage from "../imgs/custom-recipe-placeholder.png";
 import generatedRecipeImage from "../imgs/generated-recipe-placeholder.png";
 
-
 /**
  * the parent of this component creates the button options
  * @param {Recipe} meal
  * @returns
  */
 const RecipeDetails = ({ meal, buttonOptions, isOpen, saveData }) => {
-  const filteredMeal = {   
+  const filteredMeal = {
     summary: meal.summary,
     ingredients: meal.ingredients,
   };
@@ -30,57 +29,58 @@ const RecipeDetails = ({ meal, buttonOptions, isOpen, saveData }) => {
   };
 
   return (
-    <Modal isOpen={isOpen} style={{ maxWidth: "40rem" }} className="modal-window">
+    <Modal
+      isOpen={isOpen}
+      style={{ maxWidth: "40rem" }}
+      className="modal-window"
+    >
       <ModalHeader className="modal-header">{meal.name}</ModalHeader>
-      <ModalBody className="modal-body" style={{maxHeight: "25rem", overflowY: "auto"}}>
+      <ModalBody
+        className="modal-body"
+        style={{ maxHeight: "25rem", overflowY: "auto" }}
+      >
         <Container className="d-flex justify-content-center mb-3">
           <img
-            src={meal.image === "generatedRecipeImage" ? generatedRecipeImage : meal.image ? meal.image : customRecipeImage} //if the recipe is generated, use the generated meal placeholder. if it already has an image, use that. otherwise, use the custom meal placeholder
+            src={
+              meal.image === "generatedRecipeImage"
+                ? generatedRecipeImage
+                : meal.image
+                ? meal.image
+                : customRecipeImage
+            } //if the recipe is generated, use the generated meal placeholder. if it already has an image, use that. otherwise, use the custom meal placeholder
             alt={meal.name}
             style={{ maxWidth: "100%", maxHeight: "200px" }}
           />
         </Container>
         {Object.entries(filteredMeal).map(([key, value]) =>
-        key === "summary" ? (
-          <>
-            <style>
-              {`
-                b {
-                  color: var(--atomic-tangerine-darker) !important;
-                }
-                a {
-                  color: var(--air-superiority-blue-darker) !important;
-                }
-              `}
-            </style>
+          key === "summary" ? (
             <div
-              key={key}
+              key={key} // Add a unique key prop here
               style={{ wordBreak: "break-word" }}
               dangerouslySetInnerHTML={{
                 __html: DOMPurify.sanitize(value, { ALLOWED_TAGS: ["b", "a"] }),
               }}
             />
-          </>
-        ) : key === "ingredients" ? (
-          <div key={key}>
-            <strong>{key}:</strong>
-            <ul>
-              {value.map((ingredient, index) =>
-                // In case we get a bad ingredient from GPT, we perform a null check
-                ingredient ? (
-                  <li key={index}>
-                    {ingredient.amount} {ingredient.unit} {ingredient.name}
-                  </li>
-                ) : null
-              )}
-            </ul>
-          </div>
-        ) : (
-          <div key={key} style={{ wordBreak: "break-word" }}>
-            <strong>{key}:</strong> {JSON.stringify(value)}
-          </div>
-        )
-      )}
+          ) : key === "ingredients" ? (
+            <div key={key}>
+              <strong>{key}:</strong>
+              <ul>
+                {value.map((ingredient, index) =>
+                  // In case we get a bad ingredient from GPT, we perform a null check
+                  ingredient ? (
+                    <li key={index}>
+                      {ingredient.amount} {ingredient.unit} {ingredient.name}
+                    </li>
+                  ) : null
+                )}
+              </ul>
+            </div>
+          ) : (
+            <div key={key} style={{ wordBreak: "break-word" }}>
+              <strong>{key}:</strong> {JSON.stringify(value)}
+            </div>
+          )
+        )}
       </ModalBody>
       <ModalFooter className="modal-footer">
         {buttonOptions({ isClicked, cartClick, saveData })}
