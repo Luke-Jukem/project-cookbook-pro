178da69e8035b15edf1973780c9b070de8d9883c
shawndutill@gmail.com <shawndutill@gmail.com>
2024-05-12 10:39:59 -0400

    Removed all the Android shit

--- android-app/CookBookPro/app/src/main/java/com/example/cookbookpro/ui/fragmentContainers/home/homeFragments/signInFragment/SignInViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cookbookpro.ui.fragmentContainers.home.homeFragments.signInFragment
-
-import androidx.lifecycle.ViewModel
-
-class SignInViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
